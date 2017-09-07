28986f5145162a77c71e1b10d265fa41b0e10143
Patrick Brown Jr <pjbrn26@gmail.com>
2017-09-07 10:40:48 -0500

    CHecking server changes

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var app = require('./server-config.js');
+require("dotenv").config();
 
-var port = 4568;
+var port = process.env.PORT;
 
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
+console.log('Changed some stuff');
